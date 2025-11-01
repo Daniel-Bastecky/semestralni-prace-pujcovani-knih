6291e8bed98156ea5fe71f5cde2a4783328daad5
Daniel-Bastecky <dan.bastecky@gmail.com>
2025-11-01 20:02:03 +0100

    Added CRUD methods for Books, Customers and Loans

--- PujcovaniKnih/Data/Database.cs
+++ PujcovaniKnih/Data/Database.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Data.Sqlite;
+using PujcovaniKnih.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace PujcovaniKnih.Data
 {
@@ -51,6 +54,195 @@ namespace PujcovaniKnih.Data
                                     );";
             using var createLoansCmd = new SqliteCommand(createLoansTable, connection);
             createLoansCmd.ExecuteNonQuery();
+        }
+
+        public static List<Book> GetAllBooks()
+        {
+            var books = new List<Book>();
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "SELECT Id, Title, Author, IsAvailable FROM Books;";
+            using var cmd = new SqliteCommand(query, connection);
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                books.Add(new Book
+                {
+                    Id = reader.GetInt32(0),
+                    Title = reader.GetString(1),
+                    Author = reader.GetString(2),
+                    IsAvailable = reader.GetBoolean(3) == true
+                });
+            }
+
+            return books;
+        }
+
+        public static void AddBook(Book book)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "INSERT INTO Books (Title, Author, IsAvailable) VALUES (@Title,@Author,@IsAvailable);";
+            using var cmd = new SqliteCommand(query,connection);
+            cmd.Parameters.AddWithValue("@Title", book.Title);
+            cmd.Parameters.AddWithValue("@Author", book.Author);
+            cmd.Parameters.AddWithValue("@IsAvailable", book.IsAvailable ? 1 : 0);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void UpdateBook(Book book)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "UPDATE Books SET Title=@Title, Author=@Author, IsAvailable=@IsAvailable WHERE Id=@Id;";
+            using var cmd = new SqliteCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Title", book.Title);
+            cmd.Parameters.AddWithValue("@Author", book.Author);
+            cmd.Parameters.AddWithValue("@IsAvailable", book.IsAvailable ? 1 : 0);
+            cmd.Parameters.AddWithValue("@Id", book.Id);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void DeleteBook(int bookId)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "DELETE FROM Books WHERE Id=@Id;";
+            using var cmd = new SqliteCommand(query,connection);
+            cmd.Parameters.AddWithValue("@Id", bookId);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static List<Customer> GetAllCustomers()
+        {
+            var customers = new List<Customer>();
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "SELECT Id, Name, Email, Phone FROM Customers;";
+            using var cmd = new SqliteCommand(query, connection);
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                customers.Add(new Customer
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Email = reader.IsDBNull(2) ? null : reader.GetString(2),
+                    Phone = reader.GetString(3)
+                });
+            }
+
+            return customers;
+        }
+
+        public static void AddCustomer(Customer customer)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "INSERT INTO Customers (Name, Email, Phone) VALUES (@Name,@Email,@Phone);";
+            using var cmd = new SqliteCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Name", customer.Name);
+            cmd.Parameters.AddWithValue("@Email", customer.Email ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void UpdateCustomer(Customer customer)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "UPDATE Customers SET Name=@Name, Email=@Email, Phone=@Phone WHERE Id=@Id;";
+            using var cmd = new SqliteCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Name", customer.Name);
+            cmd.Parameters.AddWithValue("@Email", customer.Email ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+            cmd.Parameters.AddWithValue("@Id", customer.Id);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void DeleteCustomer(int customerId)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "DELETE FROM Customers WHERE Id=@Id;";
+            using var cmd = new SqliteCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Id", customerId);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static List<Loan> GetAllLoans()
+        {
+            var loans = new List<Loan>();
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "SELECT Id, CustomerId, BookId, DateBorrowed, DateReturned FROM Loans;";
+            using var cmd = new SqliteCommand(query, connection);
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                loans.Add(new Loan
+                {
+                    Id = reader.GetInt32(0),
+                    CustomerId = reader.GetInt32(1),
+                    BookId = reader.GetInt32(2),
+                    DateBorrowed = DateTime.Parse(reader.GetString(3)),
+                    DateReturned = reader.IsDBNull(4) ? (DateTime?)null : DateTime.Parse(reader.GetString(4))
+                });
+            }
+
+            return loans;
+        }
+
+        public static void AddLoan(Loan loan)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = @"INSERT INTO Loans (CustomerId, BookId, DateBorrowed, DateReturned) VALUES (@CustomerId,@BookId,@DateBorrowed, @DateReturned);";
+            using var cmd = new SqliteCommand(query, connection);
+            cmd.Parameters.AddWithValue("@CustomerId", loan.CustomerId);
+            cmd.Parameters.AddWithValue("@BookId", loan.BookId);
+            cmd.Parameters.AddWithValue("@DateBorrowed", loan.DateBorrowed.ToString("yyyy-MM-dd"));
+            cmd.Parameters.AddWithValue("@DateReturned", loan.DateReturned.HasValue ? loan.DateReturned.Value.ToString("yyyy-MM-dd") : (object)DBNull.Value);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void UpdateLoan(Loan loan)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = @"UPDATE Loans SET CustomerId=@CustomerId, BookId=@BookId, DateBorrowed=@DateBorrowed, DateReturned=@DateReturned WHERE Id=@Id;";
+            using var cmd = new SqliteCommand(query, connection);
+            cmd.Parameters.AddWithValue("@CustomerId", loan.CustomerId);
+            cmd.Parameters.AddWithValue("@BookId", loan.BookId);
+            cmd.Parameters.AddWithValue("@DateBorrowed", loan.DateBorrowed.ToString("yyyy-MM-dd"));
+            cmd.Parameters.AddWithValue("@DateReturned", loan.DateReturned.HasValue ? loan.DateReturned.Value.ToString("yyyy-MM-dd") : (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@Id", loan.Id);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void DeleteLoan(int loanId)
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            string query = "DELETE FROM Loans WHERE Id=@Id;";
+            using var cmd = new SqliteCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Id", loanId);
+            cmd.ExecuteNonQuery();
         }
     }
 }

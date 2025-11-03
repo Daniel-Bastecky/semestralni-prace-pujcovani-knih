b40c009643e457763086718299a77f6cb076c3c5
Daniel-Bastecky <dan.bastecky@gmail.com>
2025-11-03 16:35:22 +0100

    Added CustomersViewModel and LoansViewModel.

--- PujcovaniKnih/ViewModels/CustomersViewModel.cs
+++ PujcovaniKnih/ViewModels/CustomersViewModel.cs
@@ -1,13 +1,78 @@
-﻿using System;
+﻿using PujcovaniKnih.Data;
+using PujcovaniKnih.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PujcovaniKnih.ViewModels
 {
-    public class CustomersViewModel
+    /// <summary>
+    /// ViewModel responsible for managing customers,
+    /// providing CRUD operations and notifying the UI of changes.
+    /// </summary>
+    public class CustomersViewModel : INotifyPropertyChanged
     {
+        // Collection of customers for data binding in the UI
+        public ObservableCollection<Customer> Customers { get; set; } = new();
 
+        private Customer? selectedCustomer;
+        public Customer? SelectedCustomer
+        {
+            get => selectedCustomer;
+            set
+            {
+                selectedCustomer = value;
+                OnPropertyChanged();    // Notify the UI that the property has changed
+            }
+        }
+
+        public CustomersViewModel()
+        {
+            LoadCustomers();
+        }
+
+        public void LoadCustomers()
+        {
+            Customers.Clear();
+            var customers = Database.GetAllCustomers();
+            foreach (var customer in customers)
+            {
+                Customers.Add(customer);
+            }
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            Database.AddCustomer(customer);
+            LoadCustomers();
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                return;
+            }
+
+            Database.UpdateCustomer(customer);
+            LoadCustomers();
+        }
+
+        public void DeleteCustomer(int customerId)
+        {
+            Database.DeleteCustomer(customerId);
+            LoadCustomers();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- PujcovaniKnih/ViewModels/LoansViewModel.cs
+++ PujcovaniKnih/ViewModels/LoansViewModel.cs
@@ -1,13 +1,78 @@
-﻿using System;
+﻿using PujcovaniKnih.Data;
+using PujcovaniKnih.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PujcovaniKnih.ViewModels
 {
-    public class LoansViewModel
+    /// <summary>
+    /// ViewModel responsible for managing book loans,
+    /// providing CRUD operations and notifying the UI of changes.
+    /// </summary>
+    public class LoansViewModel : INotifyPropertyChanged
     {
+        // Collection of loans for data binding in the UI
+        public ObservableCollection<Loan> Loans { get; set; } = new();
 
+        private Loan? selectedLoan;
+        public Loan? SelectedLoan
+        {
+            get => selectedLoan;
+            set
+            {
+                selectedLoan = value;
+                OnPropertyChanged();    // Notify the UI that the property has changed
+            }
+        }
+
+        public LoansViewModel()
+        {
+            LoadLoans();
+        }
+
+        public void LoadLoans()
+        {
+            Loans.Clear();
+            var loans = Database.GetAllLoans();
+            foreach (var loan in loans)
+            {
+                Loans.Add(loan);
+            }
+        }
+
+        public void AddLoan(Loan loan)
+        {
+            Database.AddLoan(loan);
+            LoadLoans();
+        }
+
+        public void UpdateLoan(Loan loan)
+        {
+            if (loan == null)
+            {
+                return;
+            }
+
+            Database.UpdateLoan(loan);
+            LoadLoans();
+        }
+
+        public void DeleteLoan(int loanId)
+        {
+            Database.DeleteLoan(loanId);
+            LoadLoans();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }

d7d0b5a22de0f82eb3eaf3652a844f9ac3ba6ee9
Daniel-Bastecky <dan.bastecky@gmail.com>
2025-11-09 17:21:43 +0100

    Add MainViewModel.cs and RelayCommand.cs.

--- PujcovaniKnih/Commands/RelayCommand.cs
+++ PujcovaniKnih/Commands/RelayCommand.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PujcovaniKnih.Commands
 {
-    public class RelayCommand
+    /// <summary>
+    /// A simple implementation of ICommand to connect buttons in the UI with methods in the ViewModel.
+    /// </summary>
+    public class RelayCommand : ICommand
     {
+        private readonly Action<object?> execute;
+        private readonly Predicate<object?>? canExecute;
 
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove {  CommandManager.RequerySuggested -= value;}
+        }
+
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        { 
+            execute(parameter);
+        }
     }
 }
--- PujcovaniKnih/ViewModels/MainViewModel.cs
+++ PujcovaniKnih/ViewModels/MainViewModel.cs
@@ -1,13 +1,52 @@
-﻿using System;
+﻿using PujcovaniKnih.Commands;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PujcovaniKnih.ViewModels
 {
-    public class MainViewModel
+    /// <summary>
+    /// Main ViewModel that holds all other ViewModels and controls which one is currently displayed.
+    /// </summary>
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public BooksViewModel BooksVM { get; set; } = new();
+        public CustomersViewModel CustomersVM { get; set; } = new();
+        public LoansViewModel LoansVM { get; set; } = new();
 
+        private object? currentView;
+        public object? CurrentView
+        {
+            get => currentView;
+            set
+            {
+                currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Commands to switch views
+        public RelayCommand ShowBooksCommand { get; }
+        public RelayCommand ShowCustomersCommand { get; }
+        public RelayCommand ShowLoansCommand { get; }
+
+        public MainViewModel()
+        {
+            CurrentView = BooksVM;
+
+            ShowBooksCommand = new RelayCommand(_ => CurrentView = BooksVM);
+            ShowCustomersCommand = new RelayCommand(_ => CurrentView = CustomersVM);
+            ShowLoansCommand = new RelayCommand(_ => CurrentView = LoansVM);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
